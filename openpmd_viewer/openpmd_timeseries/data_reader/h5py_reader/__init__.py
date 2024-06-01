290088602ec42b535f31060d3f723c3de8c0a1c1
Ben Wibking <ben@wibking.com>
2024-06-01 17:57:01 -0400

    remove h5py_reader

--- openpmd_viewer/openpmd_timeseries/data_reader/h5py_reader/__init__.py
+++
@@ -1,5 +0,0 @@
-from .params_reader import read_openPMD_params
-from .field_reader import read_field_cartesian, get_grid_parameters
-from .utilities import list_files
-
-__all__ = ['read_openPMD_params', 'list_files', 'read_field_cartesian', 'get_grid_parameters']
