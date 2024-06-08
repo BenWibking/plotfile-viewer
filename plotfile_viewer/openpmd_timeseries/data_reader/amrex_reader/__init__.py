c26b6004ace1a1e60df5b2000466d13c81491f2b
Ben Wibking <ben@wibking.com>
2024-06-08 16:54:57 -0400

    add builtin.amrex_spacedim to set 2D/3D

--- plotfile_viewer/openpmd_timeseries/data_reader/amrex_reader/__init__.py
+++ plotfile_viewer/openpmd_timeseries/data_reader/amrex_reader/__init__.py
@@ -1,5 +1,11 @@
-# FIXME: dimension-dependent import!!
-import amrex.space3d as amr
+from builtins import amrex_spacedim
+
+if amrex_spacedim == 2:
+    import amrex.space2d as amr
+elif amrex_spacedim == 3:
+    import amrex.space3d as amr
+else:
+    raise Exception("amrex_spacedim must be 2 or 3!")
 
 from .params_reader import read_plotfile_params
 from .field_reader import read_field_cartesian, get_grid_parameters
--- plotfile_viewer/openpmd_timeseries/data_reader/amrex_reader/field_reader.py
+++ plotfile_viewer/openpmd_timeseries/data_reader/amrex_reader/field_reader.py
@@ -7,9 +7,14 @@ Copyright 2015-2016, plotfile-viewer contributors
 Author: Remi Lehe
 License: 3-Clause-BSD-LBNL
 """
+from builtins import amrex_spacedim
 
-# FIXME: dimension-dependent import!!
-import amrex.space3d as amr
+if amrex_spacedim == 2:
+    import amrex.space2d as amr
+elif amrex_spacedim == 3:
+    import amrex.space3d as amr
+else:
+    raise Exception("amrex_spacedim must be 2 or 3!")
 
 import numpy as np
 from .utilities import get_data
